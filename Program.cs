3619f922c6a343982271a4949ecbd824a1898cdf
Vladimir <vrobskiy@gmail.com>
2022-07-17 19:05:54 +0300

    task1new

--- Program.cs
+++ Program.cs
@@ -13,21 +13,7 @@ namespace Task1
             // Задача номер 1: 
             //Console.WriteLine("ЯЮниор");
 
-            // Задача номер 2:
-
-            int number = 1;
-            string name = "Vova";
-            double yPosition = 1.23;
-            char symbol = '.';
-            bool isEmployed = false;
-            float a = 3.14F;
-            char firstLetter = 'A';
-            decimal bigNumber = 1005.8M;
-            ulong price = 30UL;
-            var age = 22;
-
-
-
+         
         }
     }
 }
